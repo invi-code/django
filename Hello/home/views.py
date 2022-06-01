cde52d11283c9c14b0f9a79e5cc24a7b8f5aed74
invi-code <jeetmourya777@gmail.com>
2022-06-01 13:01:44 +0530

    first commit

---
+++ Hello/home/views.py
@@ -0,0 +1,26 @@
+from multiprocessing import context
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+def index(request):
+    context = {
+        "variable1": "this is variable1 sent",
+        "variable2": "this is variable2 sent"
+    }
+    return render(request, "index.html", context )
+
+def about(request):
+    return render(request, "about.html")
+
+    # return HttpResponse("this is about page")
+
+def services(request):
+    return render(request, "services.html" )
+
+    # return HttpResponse("this is services page")
+
+def contact(request):
+    return render(request, "contact.html")
+
+    # return HttpResponse("this is contact page")
